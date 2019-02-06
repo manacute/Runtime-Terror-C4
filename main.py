c0fd8c7f098e9f5f371d129ccf0754e516baba5b
huangm66 <maximilian.huang@mail.utoronto.ca>
2019-02-05 21:36:26 -0500

    Update main.py with MVC base
    Some pygame implementations

--- main.py
+++ main.py
@@ -7,44 +7,43 @@ University of Toronto Mississauga
 '''
 
 class Move:
-    def __init__ (self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
-        
 
 class GameBoard:
-    def __init__ (self):
+    def __init__(self):
         board = []
-    
-   
+        moves = []
+        
+    def performMove(x, y) -> null:
+        pass
+        
     def doesMoveWinGame(m: Move) -> bool:
         winrar = False
         return winrar
+
+    def ControllerTick() -> int:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return 0
+            
+            return 1
     
-
-
-
-
-
-
+    def ViewTick() -> null:
+        pygame.display.flip()    
+    
 if __name__ == '__main__':
     pygame.init()
-    size = width, height = 800, 600
-    screen = pygame.display.set_mode(size)
+    screen = pygame.display.set_mode((800, 600))
+    screen.fill((255, 255, 255))
     
     filledCross = pygame.image.load("graphics/baseline_cancel_black_48dp.png")
-    emptyCross = pygame.image.load("graphics/outline_cancel_black_48dp.png")
-    white = 255, 255, 255
-
-
-
-
+    emptyCross = pygame.image.load("graphics/outline_cancel_black_48dp.png")        
+    board = GameBoard()
+    
     while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT: sys.exit()
-
-    
-        screen.fill(white)
-        pygame.display.flip()
-
-    
+        if board.ControllerTick() == 0:
+            pygame.quit()
+            sys.exit()
+        board.ViewTick()
