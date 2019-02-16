9e3dd0be2d1476e3b85b5200ab7034264012c41f
ohmamit <amit.ohm@mail.utoronto.ca>
2019-02-16 13:12:34 -0500

    fixed spacing again

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ University of Toronto Mississauga
 '''
 
 class Piece:
-	'''Piece class to keep track of of each place on the board. On default the
-		player number is set to 0, which represents an unclaimed spot (this allows us 
-		to check if a current position of the board is empty safely).	
-	'''
-	def __init__(self, num=0: int):
-		self._player_num = num
+    '''Piece class to keep track of of each place on the board. On default the
+        player number is set to 0, which represents an unclaimed spot (this allows us 
+        to check if a current position of the board is empty safely).	
+    '''
+    def __init__(self, num=0: int):
+        self._player_num = num
 
-	def get_num(self) -> int:
-		return self._player_num
+    def get_num(self) -> int:
+        return self._player_num
 
-	def is_empty(self) -> bool:
-		return self._player_num == 0 
+    def is_empty(self) -> bool:
+        return self._player_num == 0 
 
-	def get_player(self) -> bool:
-		return self._player_num
+    def get_player(self) -> bool:
+        return self._player_num
 
 
 class Move:
