083e3ba9b9e61360fffdcf55504f4bb91844ed95
ohmamit <amit.ohm@mail.utoronto.ca>
2019-02-16 13:23:28 -0500

    added player_num attribute to move

--- main.py
+++ main.py
@@ -25,15 +25,19 @@ class Piece:
 
 
 class Move:
-    def __init__(self, x, y):
+    def __init__(self, x, y, player_num):
         self._x = x
         self._y = y
+        self._player_num = player_num
 
     def get_x(self):
         return self._x
 	
     def get_y(self):
         return self._y
+
+    def get_player(self):
+        return self._player_num
 
 class BoardModel:
     def __init__(self):
